6c154260d1fb14dfa0ce7499b1ea6cb414657ddf
tapashm42 <tapashm42@gmail.com>
2021-12-29 23:34:24 +0530

    worked on multiple component, nested componenet, @Input, @Output

--- sales_project/src/app/app.module.ts
+++ sales_project/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule,NO_ERRORS_SCHEMA,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
 
 import { AppComponent } from './app.component';
 import { SalesPersonListComponent } from './sales-person-list/sales-person-list.component';
+import { TestComponentComponent } from './test-component/test-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SalesPersonListComponent
+    SalesPersonListComponent,
+    TestComponentComponent
   ],
   imports: [
     BrowserModule
