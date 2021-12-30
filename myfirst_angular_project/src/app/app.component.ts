73bbf2cfd5b8e2b2f83ed2f471f8285fdee06ee4
tapashm42 <tapashm42@gmail.com>
2021-12-31 00:23:53 +0530

    Added few more products in the list & corrected the issue inproduct list  design

---
+++ myfirst_angular_project/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Tapas\'s first angular';
+  date = '21st Dec, 2021'
+  time = '10:59:19 AM'
+}
